9ac18e718211026fa3d5b541ed66be97ea28da7f
Vincent Chang <vincenttchang90@gmail.com>
2017-09-14 19:21:55 -0400

    added star functionality

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ let deck = $('.card');
 let open = [];
 let matched = [];
 let count;
+let start;
+let end;
 
 function newGame() {
     shuffle(deck);
@@ -23,6 +25,9 @@ function newGame() {
     count = 0;
     $('.moves').text(count);
     open = [];
+
+    $('.stars').empty();
+    $('.stars').append('<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>');
 
     $(deck).each(function(card){
         $(deck[card]).attr('class', 'card');
@@ -40,6 +45,9 @@ function clicker(card) {
     if (open.length == 0) {
         card.off();
         open.push(card);
+        if (count == 0) {
+            start = new Date();
+        }
     }else {
         $('.card').off();
         open.push(card);
@@ -55,6 +63,12 @@ function clicker(card) {
 function increaseScore(){
     count++;
     $('.moves').text(count);
+    if (count == 10) {
+        $('.fa-star').first().remove();
+    }
+    if (count == 16) {
+        $('.fa-star').first().remove();
+    }
 }
 
 function checkMatch(){
@@ -67,7 +81,8 @@ function checkMatch(){
         }
 
         if (matched.length == cardsArray.length) {
-            console.log('CONGRATS YOU WON IN '+ count + " MOVES");
+            end = new Date();
+            console.log('CONGRATS YOU WON IN '+ count + ' MOVES! IT TOOK '+(end-start)/1000+" seconds");
         }
         open.length = 0;
     }else {
